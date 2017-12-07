1c2ed148b363fa268b8faf8352a421669094f2ec
William Wong <compulim@hotmail.com>
2017-12-07 15:36:36 -0800

    Support BlobService in addition to account and access key

---
+++ test/constructor-test.js
@@ -0,0 +1,42 @@
+'use strict'
+
+require('dotenv').config();
+
+const assert       = require('assert');
+const AzureBlobFS  = require('../lib/AzureBlobFS');
+const azureStorage = require('azure-storage');
+
+const FILENAME = 'constructor.txt';
+
+describe.only('constructor', () => {
+  let fs, helper;
+
+  before(async () => {
+    await require('./createAzureBlobFS')();
+
+    fs = new AzureBlobFS(azureStorage.createBlobService(), process.env.TEST_CONTAINER);
+
+    helper = require('./testHelper')(fs.promise);
+  });
+
+  beforeEach(async () => {
+    await helper.ensureUnlinkIfExists(FILENAME);
+    await helper.ensureWriteFile(FILENAME, 'Hello, World!');
+  });
+
+  afterEach(async () => {
+    await helper.ensureUnlinkIfExists(FILENAME);
+  });
+
+  describe('read/write a text file', () => {
+    let content;
+
+    beforeEach(async () => {
+      content = await fs.promise.readFile(FILENAME);
+    });
+
+    it('should return the content of the file', () => {
+      assert.equal(content.toString(), 'Hello, World!');
+    });
+  });
+});
