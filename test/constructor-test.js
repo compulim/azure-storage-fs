80f037b2388448add46fedcc9613b825deafc0b6
William Wong <compulim@hotmail.com>
2017-12-07 15:42:14 -0800

    Add null account and access key

--- test/constructor-test.js
+++ test/constructor-test.js
@@ -8,35 +8,70 @@ const azureStorage = require('azure-storage');
 
 const FILENAME = 'constructor.txt';
 
-describe('constructor', () => {
-  let fs, helper;
+describe.only('constructor', () => {
+  describe('construct using BlobService object', () => {
+    let fs, helper;
 
-  before(async () => {
-    await require('./createAzureBlobFS')();
+    before(async () => {
+      await require('./createAzureBlobFS')();
 
-    fs = new AzureBlobFS(azureStorage.createBlobService(), process.env.TEST_CONTAINER);
+      fs = new AzureBlobFS(azureStorage.createBlobService(), process.env.TEST_CONTAINER);
 
-    helper = require('./testHelper')(fs.promise);
-  });
-
-  beforeEach(async () => {
-    await helper.ensureUnlinkIfExists(FILENAME);
-    await helper.ensureWriteFile(FILENAME, 'Hello, World!');
-  });
-
-  afterEach(async () => {
-    await helper.ensureUnlinkIfExists(FILENAME);
-  });
-
-  describe('read/write a text file', () => {
-    let content;
-
-    beforeEach(async () => {
-      content = await fs.promise.readFile(FILENAME);
+      helper = require('./testHelper')(fs.promise);
     });
 
-    it('should return the content of the file', () => {
-      assert.equal(content.toString(), 'Hello, World!');
+    beforeEach(async () => {
+      await helper.ensureUnlinkIfExists(FILENAME);
+      await helper.ensureWriteFile(FILENAME, 'Hello, World!');
+    });
+
+    afterEach(async () => {
+      await helper.ensureUnlinkIfExists(FILENAME);
+    });
+
+    describe('read/write a text file', () => {
+      let content;
+
+      beforeEach(async () => {
+        content = await fs.promise.readFile(FILENAME);
+      });
+
+      it('should return the content of the file', () => {
+        assert.equal(content.toString(), 'Hello, World!');
+      });
+    });
+  });
+
+  describe('construct by letting BlobService to read from environment variable', () => {
+    let fs, helper;
+
+    before(async () => {
+      await require('./createAzureBlobFS')();
+
+      fs = new AzureBlobFS(null, null, process.env.TEST_CONTAINER);
+
+      helper = require('./testHelper')(fs.promise);
+    });
+
+    beforeEach(async () => {
+      await helper.ensureUnlinkIfExists(FILENAME);
+      await helper.ensureWriteFile(FILENAME, 'Hello, World!');
+    });
+
+    afterEach(async () => {
+      await helper.ensureUnlinkIfExists(FILENAME);
+    });
+
+    describe('read/write a text file', () => {
+      let content;
+
+      beforeEach(async () => {
+        content = await fs.promise.readFile(FILENAME);
+      });
+
+      it('should return the content of the file', () => {
+        assert.equal(content.toString(), 'Hello, World!');
+      });
     });
   });
 });
