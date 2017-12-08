9b04b59209ca45d3454b8d26f41280388dd6d249
William Wong <compulim@hotmail.com>
2017-12-07 21:31:36 -0800

    Move away from doWhilst/whilst

---
+++ __tests__/blob/rename.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const FILENAME1 = 'rename1.txt';
+const FILENAME2 = 'rename2.txt';
+
+describe('rename', () => {
+  let fs, helper;
+
+  beforeEach(async () => {
+    fs     = await require('../../testUtils/createAzureBlobFS')();
+    helper = require('../../testUtils/testHelper')(fs.promise);
+
+    await Promise.all([FILENAME1, FILENAME2].map(file => helper.ensureUnlinkIfExists(file)));
+    await helper.ensureWriteFile(FILENAME1, 'Hello, World!');
+  });
+
+  afterEach(async () => {
+    await Promise.all([FILENAME1, FILENAME2].map(file => helper.ensureUnlinkIfExists(file)));
+  });
+
+  test('rename a file', async () => {
+    await fs.promise.rename(FILENAME1, FILENAME2);
+    await Promise.all([
+      helper.ensureNotExists(FILENAME1),
+      helper.ensureExists(FILENAME2)
+    ]);
+
+    const buffer = await fs.promise.readFile(FILENAME2);
+
+    expect(buffer.toString()).toBe('Hello, World!');
+  });
+});
