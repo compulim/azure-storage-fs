0d5a46ea4f394905a2b6c8797587df017f1db72d
Nathan Davis <ndavis@reachmedianetwork.com>
2020-03-27 12:38:14 -0500

    expose createWriteStream and createReadStream - expose the createWriteStream and createReadStream functions - export methods and constants used by these functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 'use strict';
 
-const AzureBlobFS = require('./AzureBlobFS');
+const {
+  AzureBlobFS, normalizePath, DEFAULT_WRITE_FILE_OPTIONS,
+  DEFAULT_READ_FILE_OPTIONS, DEFAULT_OPTIONS
+} = require('./AzureBlobFS');
 
 module.exports = {
-  blob: (account, secret, container) => new AzureBlobFS(account, secret, container)
+  blob: (account, secret, container) => new AzureBlobFS(account, secret, container),
+  normalizePath: normalizePath,
+  DEFAULT_WRITE_FILE_OPTIONS: DEFAULT_WRITE_FILE_OPTIONS,
+  DEFAULT_READ_FILE_OPTIONS: DEFAULT_READ_FILE_OPTIONS,
+  DEFAULT_OPTIONS: DEFAULT_OPTIONS
 };
